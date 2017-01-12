03fecdafa7bce1dd3c8949a018023f8c08c4b686
tiegan <tiegan@ualberta.ca>
2017-01-11 18:02:06 -0700

    Lab 1 new

--- checkversion.py
+++ checkversion.py
@@ -4,7 +4,11 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com")
+#response = requests.get("http://google.com")
+
+#print response.text
+#print response.status_code
+
+response = requests.get("https://raw.githubusercontent.com/tiegan/CMPUT404/master/checkversion.py")
 
 print response.text
-print response.status_code
